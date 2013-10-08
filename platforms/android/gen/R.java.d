c557b2683fb1bed8ba3b0ee2513eb257dea37389
Mike Almond <mike.almond@madebypi.co.uk>
2013-10-08 11:48:08 +0100

    added all plugins, built android project

---
+++ platforms/android/gen/R.java.d
@@ -0,0 +1,9 @@
+C:\Work\madebypi.github\phonegap-helloworld\platforms\android\gen\com\phonegap\helloworld\R.java \
+ : C:\Work\madebypi.github\phonegap-helloworld\platforms\android\res\drawable\icon.png \
+C:\Work\madebypi.github\phonegap-helloworld\platforms\android\res\drawable-hdpi\icon.png \
+C:\Work\madebypi.github\phonegap-helloworld\platforms\android\res\drawable-ldpi\icon.png \
+C:\Work\madebypi.github\phonegap-helloworld\platforms\android\res\drawable-mdpi\icon.png \
+C:\Work\madebypi.github\phonegap-helloworld\platforms\android\res\drawable-xhdpi\icon.png \
+C:\Work\madebypi.github\phonegap-helloworld\platforms\android\res\values\strings.xml \
+C:\Work\madebypi.github\phonegap-helloworld\platforms\android\res\xml\config.xml \
+C:\Work\madebypi.github\phonegap-helloworld\platforms\android\bin\AndroidManifest.xml \
